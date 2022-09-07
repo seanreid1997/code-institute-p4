a591439bda8835169898caefac6ca42632ecdc4c
seanreid1997 <reid.sean14@yahoo.com>
2022-09-07 17:59:33 +0000

    Create Models for posts and comments

--- social/models.py
+++ social/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Posted'))
+
+
+class Post(models.Model):
+
+    """
+    Class for posts added to databse(temporary docstring)
+    """
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    featured_image = CloudinaryField('image', default='placeholder')
+    user_text = models.TextField()
+    exerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comments(models.Model):
+    """
+    Class for comments added to database(temporary docstring)
+    """
+    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment {self.text} by {self.name}"
